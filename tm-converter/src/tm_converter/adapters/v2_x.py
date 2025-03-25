94e1e180fddbbc42d202adc497afd864483bfec1
liutao-kx <120848919@qq.com>
2025-03-25 10:34:27 +0800

    dev

--- tm-converter/src/tm_converter/adapters/v2_x.py
+++ tm-converter/src/tm_converter/adapters/v2_x.py
@@ -7,7 +7,7 @@ class V2xAdapter(BaseConverter):
     def to_intermediate(cls, raw_data):
         # 处理v2.x版本特性
         return IntermediateModel(
-            version=raw_data['version'],
+            version=raw_data.get('version', ''),
             profile=raw_data['profile'],
             properties=[cls._convert_specs(p) for p in raw_data['properties']],
             services=raw_data['services'],
@@ -17,13 +17,16 @@ class V2xAdapter(BaseConverter):
     @staticmethod
     def _convert_specs(prop):
         # 处理step字段null值
-        if prop['dataType']['specs'].get('step') is None:
-            prop['dataType']['specs']['step'] = ""
+        specs = prop.get('dataType', {}).get('specs', {})
+        if not isinstance(specs, dict):
+            specs = {}
+            prop['dataType'] = {'specs': specs}
+        specs['step'] = "" if specs.get('step') is None else specs['step']
         return PropertyModel(**prop)
 
     @classmethod
     def from_intermediate(cls, intermediate):
         output = intermediate.dict(exclude_unset=True)
         # 保持v2.x格式
-        output['profile']['stTmId'] = ""  # 添加v2.x特有字段
+        output.profile.stTmId = ""  # 添加v2.x特有字段
         return output
