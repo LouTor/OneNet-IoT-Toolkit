52bc789fc1f53593b6e1d1ba3d58175d9cadd1d9
liutao-kx <120848919@qq.com>
2025-04-02 15:31:46 +0800

    dev

--- tm_converter/src/tm_converter/core/converters/base_converter.py
+++ tm_converter/src/tm_converter/core/converters/base_converter.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod
 from tm_converter.src.tm_converter.core.models.intermediate import IntermediateModel
 
 
-# 笔记：abstract装饰器强制子类实现抽象基类定义的方法，不提供具体实现；子类未实现抽象方法会抛出TypeError
 class BaseConverter(ABC):
     @staticmethod
     def to_intermediate(raw_data: dict) -> IntermediateModel:
